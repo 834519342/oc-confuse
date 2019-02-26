72d82188d52a21d36dbd9edaeb7d7455481a6085
TJ <834519342@qq.com>
2019-02-26 17:32:07 +0800

    添加属性混淆

--- OC_class_define.py
+++ OC_class_define.py
@@ -2,7 +2,8 @@
 # -*- coding:utf-8 -*-
 
 '''
-
+Pattern 英 ['pæt(ə)n] n. 模式；图案；样品
+variable 英 ['veərɪəb(ə)l] n. [数] 变量；可变物，可变因素
 '''
 
 # 正则表达式
@@ -11,7 +12,9 @@
 # \w+ >=1个的字母数字下划线
 # \s* 贪婪模式，最大长度的匹配
 # \s*? 非贪婪模式，取最小的长度，最少0个
-# \(.+?\) 匹配以(开头,以)结尾的字符串
+# \(.+?\),\(.*?\) 匹配以(开头,以)结尾的字符串
+
+# findall(string[, pos[, endpos]])  找到所有匹配的子串，返回一个列表
 
 import os
 import json
@@ -24,9 +27,10 @@ import re
 
 # 类名白名单
 class_ignore = ('AppDelegate', 'main')
-
-# 方法白名单
+# 方法名白名单
 func_ignore = ('viewDidLoad')
+# 属性名白名单
+variable_ignore = ('window')
 
 
 # 设置默认编码格式
@@ -44,12 +48,14 @@ define_header_path = os.path.join(script_path, 'define_header')
 # 保证唯一性
 classNames = set()
 funcNames = set()
+variableNames = set()
 defineNames = set()
 
 # 正则表达式
 classPattern = re.compile('@interface\s+(\w+)\s+:\s+\w+')
 funcPattern = re.compile('\s*-\s*\(.+?\)\s*(\w+)')
-
+variablePattern = re.compile('@property\s*\(.*?\)\s*\w+\s*\*?\s*(\w+?);')
+variablePattern1 = re.compile('\s*\w+\s*\*\s*(\w+);')
 
 # 获取单词库
 with open(os.path.join(script_path, 'word_list.json'), 'r') as fileObjc:
@@ -102,6 +108,7 @@ def scan_folder_class(parent_path):
                     fileObjc.close()
                     for className in classNameList:
                         if not className in classNames and not className in class_ignore:
+                            print '类名：' + className
                             classNames.add(className)
 
 
@@ -125,14 +132,48 @@ def scan_folder_func(parent_path):
                 continue
 
             if filetype == '.h' or filetype == '.m' or filetype == '.mm':
-                print fileName
                 with open(os.path.join(parent, fileName), 'r') as fileObjc:
                     # 方法名表达式匹配
                     funcNameList = funcPattern.findall(fileObjc.read())
                     fileObjc.close()
                     for funcName in funcNameList:
                         if not funcName in funcNames and not funcName in func_ignore:
+                            print '方法名：' + funcName
                             funcNames.add(funcName)
+
+
+# ------------------------ 扫描属性对象参数 -------------------------
+def scan_folder_variable(parent_path):
+    global variableNames, variablePattern
+    variableNames.clear()
+
+    if not os.path.isdir(parent_path):
+        print '目录不存在'
+        exit(0)
+
+    # 遍历目录
+    for parent, folders, files in os.walk(parent_path):
+        # 筛选指定类型的文件
+        for fileName in files:
+            (name, filetype) = os.path.splitext(fileName)
+            if filetype == '.h' or filetype == '.m' or filetype == '.mm':
+                with open(os.path.join(parent, fileName), 'r') as fileObjc:
+                    # 属性名表达式匹配
+                    lines = fileObjc.readlines()
+                    for line_text in lines:
+                        variableNameList = variablePattern.findall(line_text)
+                        if len(variableNameList) > 0:
+                            for variableName in variableNameList:
+                                if not variableName in variableNames and not variableName in variable_ignore:
+                                    print '属性名：' + variableName
+                                    variableNames.add(variableName)
+                        else:
+                            variableNameList = variablePattern1.findall(line_text)
+                            if len(variableNameList) > 0:
+                                for variableName in variableNameList:
+                                    if not variableName in variableNames and not variableName in variable_ignore:
+                                        print '属性名：' + variableName
+                                        variableNames.add(variableName)
 
 
 # ------------------------ 执行 --------------------------------
@@ -162,6 +203,10 @@ if __name__ == '__main__':
 
     # 扫描需要处理的方法名
     scan_folder_func(args.path)
-    print funcNames
     # 添加宏混淆
     add_define_class(os.path.join(define_header_path, headerName), funcNames)
+
+    # 扫描需要处理的属性名
+    scan_folder_variable(args.path)
+    # 添加宏混淆
+    add_define_class(os.path.join(define_header_path, headerName), variableNames)
