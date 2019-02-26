f0b6b782821cd86c2e9a91dea4a91b1fdfb22b06
TJ <834519342@qq.com>
2019-02-26 14:55:47 +0800

    更新，处理方法名

--- OC_class_define.py
+++ OC_class_define.py
@@ -5,7 +5,13 @@
 
 '''
 
-#
+# 正则表达式
+# () 取值
+# \s+ >=1个的空格
+# \w+ >=1个的字母数字下划线
+# \s* 贪婪模式，最大长度的匹配
+# \s*? 非贪婪模式，取最小的长度，最少0个
+# \(.+?\) 匹配以(开头,以)结尾的字符串
 
 import os
 import json
@@ -14,10 +20,14 @@ import random
 import string
 import shutil
 import argparse
+import re
 
+# 类名白名单
+class_ignore = ('AppDelegate', 'main')
 
-# 不处理白名单
-class_ignore = ('ViewController', 'AppDelegate')
+# 方法白名单
+func_ignore = ('viewDidLoad')
+
 
 # 设置默认编码格式
 default_encoding = 'utf-8'
@@ -33,7 +43,13 @@ define_header_path = os.path.join(script_path, 'define_header')
 
 # 保证唯一性
 classNames = set()
+funcNames = set()
 defineNames = set()
+
+# 正则表达式
+classPattern = re.compile('@interface\s+(\w+)\s+:\s+\w+')
+funcPattern = re.compile('\s*-\s*\(.+?\)\s*(\w+)')
+
 
 # 获取单词库
 with open(os.path.join(script_path, 'word_list.json'), 'r') as fileObjc:
@@ -44,14 +60,16 @@ with open(os.path.join(script_path, 'word_list.json'), 'r') as fileObjc:
 # 获取一个随机名
 def get_one_name():
     global word_names
-    return random.choice(word_names) + ''.join(random.sample(string.ascii_letters, 1))
+    return ''.join(random.sample(string.ascii_uppercase, 1)) + random.choice(word_names)
 
-# ------------------------ 混淆类名 -----------------------------
+
 # 添加宏到.h文件
-def add_define(header_path, classNameList):
+def add_define_class(header_path, nameList):
     global defineNames
-    with open(header_path, 'w') as fileObjc:
-        for className in classNameList:
+    defineNames.clear()
+
+    with open(header_path, 'a+') as fileObjc:
+        for className in nameList:
             # 判断唯一性
             defineName = get_one_name()
             while defineName in defineNames:
@@ -62,24 +80,59 @@ def add_define(header_path, classNameList):
         fileObjc.close()
 
 
+# ------------------------ 扫描类名 -----------------------------
 # 扫描指定目录的类名
-def scan_folder(parent_path):
-    global classNames
-    if not os.path.exists(parent_path):
+def scan_folder_class(parent_path):
+    global classNames, class_ignore, classPattern
+    classNames.clear()
+
+    if not os.path.isdir(parent_path):
         print '目录不存在'
         exit(0)
-
-    classNames.clear()
 
     # 遍历目录
     for parent, folders, files in os.walk(parent_path):
         # 筛选.h文件
         for fileName in files:
-            index = fileName.rfind('.h')
-            if index != -1:
-                fileName = fileName[:index]
-                if not fileName in classNames and not fileName in class_ignore:
-                    classNames.add(fileName)
+            (name, ftype) = os.path.splitext(fileName)
+            if ftype == '.h':
+                with open(os.path.join(parent, fileName), 'r') as fileObjc:
+                    # 类名表达式匹配
+                    classNameList = classPattern.findall(fileObjc.read())
+                    fileObjc.close()
+                    for className in classNameList:
+                        if not className in classNames and not className in class_ignore:
+                            classNames.add(className)
+
+
+# ----------------------- 扫描方法名 -----------------------------
+# 扫描指定目录的方法
+def scan_folder_func(parent_path):
+    global funcNames, class_ignore, funcPattern, func_ignore
+    funcNames.clear()
+
+    if not os.path.isdir(parent_path):
+        print '目录不存在'
+        exit(0)
+
+    # 遍历目录
+    for parent, folders, files in os.walk(parent_path):
+        # 筛选.h和.m和.mm文件
+        for fileName in files:
+            # 跳过白名单
+            (name, filetype) = os.path.splitext(fileName)
+            if name in class_ignore:
+                continue
+
+            if filetype == '.h' or filetype == '.m' or filetype == '.mm':
+                print fileName
+                with open(os.path.join(parent, fileName), 'r') as fileObjc:
+                    # 方法名表达式匹配
+                    funcNameList = funcPattern.findall(fileObjc.read())
+                    fileObjc.close()
+                    for funcName in funcNameList:
+                        if not funcName in funcNames and not funcName in func_ignore:
+                            funcNames.add(funcName)
 
 
 # ------------------------ 执行 --------------------------------
@@ -99,7 +152,16 @@ if __name__ == '__main__':
     os.mkdir(define_header_path)
     # 获取参数
     args = parse_args()
+    # 创建一个头文件名字
+    headerName = get_one_name() + '.h'
+
     # 扫描需要处理的类名
-    scan_folder(args.path)
-    # 添加宏命令
-    add_define(os.path.join(define_header_path, get_one_name() + '.h'), classNames)
+    scan_folder_class(args.path)
+    # 添加宏混淆
+    add_define_class(os.path.join(define_header_path, headerName), classNames)
+
+    # 扫描需要处理的方法名
+    scan_folder_func(args.path)
+    print funcNames
+    # 添加宏混淆
+    add_define_class(os.path.join(define_header_path, headerName), funcNames)
